8f9c2b4b81d0fe2477ea75088ee2ddd088e33253
solutena <solutena@users.noreply.github.com>
2026-05-13 21:24:40 +0900

    1.4.3 - 옵저블 딕셔너리 개선

--- Runtime/ObservableDictionary.cs
+++ Runtime/ObservableDictionary.cs
@@ -4,40 +4,28 @@ using System.Collections.Generic;
 
 public class ObservableDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
 {
-	public readonly struct Change
-	{
-		public enum Type
-		{
-			Add,
-			Update,
-			Remove,
-		}
-
-		public readonly TKey Key;
-		public readonly TValue PreviousValue;
-		public readonly TValue CurrentValue;
-		public readonly Type ChangeType;
-
-		public Change(TKey key, TValue previousValue, TValue currentValue, Type type)
-		{
-			Key = key;
-			PreviousValue = previousValue;
-			CurrentValue = currentValue;
-			ChangeType = type;
-		}
-	}
-
 	private readonly Dictionary<TKey, TValue> _dict = new();
 
-	public delegate void ChangedHandler(Change change);
-	public event ChangedHandler OnChanged;
+	public delegate void AddedHandler(TKey key, TValue value);
+	public delegate void RemovedHandler(TKey key, TValue value);
+	public delegate void ReplacedHandler(TKey key, TValue previous, TValue current);
 
-	public TValue this[TKey key] => _dict[key];
+	public event AddedHandler OnAdded;
+	public event RemovedHandler OnRemoved;
+	public event ReplacedHandler OnReplaced;
+
+	public TValue this[TKey key]
+	{
+		get => _dict[key];
+		set => Set(key, value);
+	}
 	public int Count => _dict.Count;
 	public bool ContainsKey(TKey key) => _dict.ContainsKey(key);
 	public bool TryGetValue(TKey key, out TValue value) => _dict.TryGetValue(key, out value);
 	public Dictionary<TKey, TValue>.KeyCollection Keys => _dict.Keys;
 	public Dictionary<TKey, TValue>.ValueCollection Values => _dict.Values;
+	public TValue GetValueOrDefault(TKey key) => _dict.TryGetValue(key, out var value) ? value : default;
+	public TValue GetValueOrDefault(TKey key, TValue defaultValue) => _dict.TryGetValue(key, out var value) ? value : defaultValue;
 	public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _dict.GetEnumerator();
 	IEnumerator IEnumerable.GetEnumerator() => _dict.GetEnumerator();
 
@@ -48,49 +36,54 @@ public class ObservableDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey,
 
 		_dict.Add(key, value);
 
-		if (!isNotify)
-			return true;
+		if (isNotify)
+			OnAdded?.Invoke(key, value);
+		return true;
+	}
 
-		OnChanged?.Invoke(new Change(key, default, value, Change.Type.Add));
+	public bool TryAdd(TKey key, TValue value, bool isNotify = true)
+	{
+		if (_dict.ContainsKey(key))
+			return false;
+
+		_dict.Add(key, value);
+
+		if (isNotify)
+			OnAdded?.Invoke(key, value);
 		return true;
 	}
 
 	public bool Set(TKey key, TValue value, bool isNotify = true)
 	{
-		if (_dict.TryGetValue(key, out TValue prevValue))
+		if (_dict.TryGetValue(key, out TValue prev))
 		{
-			if (EqualityComparer<TValue>.Default.Equals(prevValue, value))
+			if (EqualityComparer<TValue>.Default.Equals(prev, value))
 				return false;
 
 			_dict[key] = value;
 
-			if (isNotify == false)
-				return true;
-
-			OnChanged?.Invoke(new Change(key, prevValue, value, Change.Type.Update));
+			if (isNotify)
+				OnReplaced?.Invoke(key, prev, value);
 			return true;
 		}
 
 		_dict.Add(key, value);
 
-		if (isNotify == false)
-			return true;
-
-		OnChanged?.Invoke(new Change(key, default, value, Change.Type.Add));
+		if (isNotify)
+			OnAdded?.Invoke(key, value);
 		return true;
 	}
 
-	public bool Remove(TKey key, bool isNotify = true)
+	public bool Remove(TKey key, bool isNotify = true) => Remove(key, out _, isNotify);
+	public bool Remove(TKey key, out TValue value, bool isNotify = true)
 	{
-		if (!_dict.TryGetValue(key, out TValue value))
+		if (!_dict.TryGetValue(key, out value))
 			return false;
 
 		_dict.Remove(key);
 
-		if (isNotify == false)
-			return true;
-
-		OnChanged?.Invoke(new Change(key, value, default, Change.Type.Remove));
+		if (isNotify)
+			OnRemoved?.Invoke(key, value);
 		return true;
 	}
 
@@ -110,7 +103,7 @@ public class ObservableDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey,
 		for (int i = 0; i < buffer.Count; i++)
 		{
 			var pair = buffer[i];
-			OnChanged?.Invoke(new Change(pair.Key, pair.Value, default, Change.Type.Remove));
+			OnRemoved?.Invoke(pair.Key, pair.Value);
 		}
 	}
 }
